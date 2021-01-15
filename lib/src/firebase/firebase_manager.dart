a0ca3b8f3d61f49903ca59caf96b5e69fd64627b
Dhaval Patel <dhavalpatel244@gmail.com>
2021-01-15 13:45:17 +0530

    Added platform types in analytics

--- lib/src/firebase/firebase_manager.dart
+++ lib/src/firebase/firebase_manager.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:firebase_analytics/firebase_analytics.dart';
 
 class FirebaseManager {
@@ -6,21 +8,46 @@ class FirebaseManager {
   void logViewEvent(String event) {
     analytics.logEvent(
       name: 'view_item',
-      parameters: {"content_type": event},
+      parameters: {
+        "content_type": event,
+        "platform": _platform(),
+      },
     );
   }
 
   void logSelectContentEvent(String event) {
     analytics.logEvent(
       name: 'select_content',
-      parameters: {"content_type": event},
+      parameters: {
+        "content_type": event,
+        "platform": _platform(),
+      },
     );
   }
 
   void logDarkModeEvent({bool enabled = false}) {
     analytics.logEvent(
       name: 'dark_mode',
-      parameters: {"status": enabled},
+      parameters: {
+        "status": enabled,
+        "platform": _platform(),
+      },
     );
+  }
+
+  String _platform() {
+    if (Platform.isAndroid) {
+      return "android";
+    } else if (Platform.isIOS) {
+      return "ios";
+    } else if (Platform.isWindows) {
+      return "windows_os";
+    } else if (Platform.isMacOS) {
+      return "mac_os";
+    } else if (Platform.isLinux) {
+      return "linux";
+    } else {
+      return "web";
+    }
   }
 }
