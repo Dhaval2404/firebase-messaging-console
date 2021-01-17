2840c31eebefc01386aed2119cc2011a96a5bb36
Dhaval Patel <dhavalpatel244@gmail.com>
2021-01-17 23:11:19 +0530

    Initialize FlutterFire

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
@@ -7,8 +8,9 @@ import 'res/theme.dart';
 import 'src/screens/main_screen.dart';
 import 'src/util/theme_notifier.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 
   runApp(
     ChangeNotifierProvider<ThemeNotifier>(
